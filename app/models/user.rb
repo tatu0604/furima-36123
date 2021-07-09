cd8aab479ff763f9259003aa523f0b302ac8b2f9
tatu0604 <26532653h@gmail.com>
2021-07-09 19:22:01 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :surname, presence: true
+  validates :first_name, presence: true
+  validates :kana_surname, presence: true
+  validates :kana_first_name, presence: true
+  validates :birthday, presence: true
+
+  # 正規表現の記述がわかりません、、、
+
+  has_many :items
+  has_many :buys
 end
