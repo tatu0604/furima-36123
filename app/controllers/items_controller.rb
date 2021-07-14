c8a15ed6d1ea417fb397f86d415d10c7437b4d8d
tatu0604 <26532653h@gmail.com>
2021-07-14 19:04:48 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :information, :category_id, :status_id, :shipping_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :information, :category_id, :status_id, :shipping_id, :area_id, :days_id,
+                                 :price).merge(user_id: current_user.id)
   end
 end
